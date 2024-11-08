717d434bbbe586965513c0442a5d2326617f022a
Augustas Pocius <nesuspameris@gmail.com>
2024-11-08 13:46:32 +0200

    move some some css rules to separate files, add BEM naming, add some accessibility, SEO improvements

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import './style.css'
+import './styles/header.css'
+import './styles/page-content.css'
+import './styles/password-generator.css'
+import './styles/info-section.css'
 import './reset.css';
 
 function generatePassword(includeUpperCase: boolean, includeLowerCase: boolean, includeNumbers: boolean, includeSymbols: boolean): string {
@@ -26,13 +30,13 @@ function generatePassword(includeUpperCase: boolean, includeLowerCase: boolean, 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const passwordInput = document.querySelector('.password-generator--input') as HTMLInputElement;
+    const passwordInput = document.querySelector('.password-generator__input') as HTMLInputElement;
     const upperCaseCheckbox = document.getElementById('include-upper-case') as HTMLInputElement;
     const lowerCaseCheckbox = document.getElementById('include-lower-case') as HTMLInputElement;
     const numbersCheckbox = document.getElementById('include-numbers') as HTMLInputElement;
     const symbolsCheckbox = document.getElementById('include-symbols') as HTMLInputElement;
-    const regeneratePasswordBtn = document.querySelector('.regenerate-password') as HTMLButtonElement;
-    const copyBtn = document.querySelector('.copy-to-clipboard') as HTMLButtonElement;
+    const regeneratePasswordBtn = document.querySelector('.password-generator__button--regenerate') as HTMLButtonElement;
+    const copyBtn = document.querySelector('.password-generator__button--copy') as HTMLButtonElement;
 
     const updatePassword = () => {
         const includeUpperCase = upperCaseCheckbox.checked;
